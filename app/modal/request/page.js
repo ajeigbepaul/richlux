37defdbdddc387666f8126fcbaed627ff8582479
ajeigbepaul <ajeigbepaul@yahoo.com>
2023-08-21 00:44:41 +0100

    added the make request button and the request modal

---
+++ app/modal/request/page.js
@@ -0,0 +1,87 @@
+import Input from "@/components/Input";
+import SubmitButton from "@/components/SubmitButton";
+import React, { useState } from "react";
+
+function RequestModal({ visible, handleClose, CloseIcon }) {
+  if (!visible) return null;
+  const [isLoading, setIsLoading] = useState(false);
+  return (
+    <div className="md:w-3/5 w-full h-[75%] bg-gray-800 absolute  top-24 md:top-20 z-50 rounded-md p-2">
+      <div className="md:flex md:space-x-2 w-full">
+        <div className="flex flex-col bg-gray-900 w-full h-full text-white">
+          <div className="flex justify-between items-center w-full md:p-2 p-2 bg-orange-400">
+            <h2 className=" text-white font-medium">
+              Personal Information
+            </h2>
+            <div
+              className="w-8 h-8 bg-white rounded-full md:hidden flex items-center justify-center shadow-lg"
+              onClick={handleClose}
+            >
+              <CloseIcon size={24} className="font-bold cursor-pointer" color="red" />
+            </div>
+          </div>
+          <form className="flex flex-col  w-full p-4 space-y-3">
+            <Input
+              type="text"
+              placeholder="Fullname"
+              className="p-1 text-gray-700 placeholder:text-orange-400 placeholder:text-sm"
+            />
+            <Input
+              type="email"
+              placeholder="Email"
+              className="p-1 text-gray-700 placeholder:text-orange-400 placeholder:text-sm"
+            />
+            <Input
+              type="text"
+              placeholder="Phone number"
+              className="p-1 text-gray-700 placeholder:text-orange-400 placeholder:text-sm"
+            />
+            <Input
+              type="text"
+              placeholder="Present location"
+              className="p-1 text-gray-700 placeholder:text-orange-400 placeholder:text-sm"
+            />
+            <Input
+              type="text"
+              placeholder="Sex"
+              className="p-1 text-gray-700 placeholder:text-orange-400 placeholder:text-sm"
+            />
+            <Input
+              type="text"
+              placeholder="Budget"
+              className="p-1 text-gray-700 placeholder:text-orange-400 placeholder:text-sm"
+            />
+          </form>
+        </div>
+        <div className="w-full bg-orange-400">
+          <div className="flex justify-between items-center w-full md:p-2 p-2">
+            <h2 className=" bg-orange-400 text-white font-bold">
+              Text Us Your Taste.
+            </h2>
+            <div
+              className="w-8 h-8 bg-white rounded-full hidden md:flex items-center justify-center shadow-lg"
+              onClick={handleClose}
+            >
+              <CloseIcon size={24} className="font-bold cursor-pointer" />
+            </div>
+          </div>
+          <form className="md:p-4 p-2 w-full">
+            <textarea
+              // rows={7}
+              // cols={40}
+              className="p-2 md:w-96 md:h-44 w-full"
+              placeholder="Eg. I am looking for a mini-flat at Oluyole..."
+            />
+            <SubmitButton
+              title="Place Request"
+              className="w-full bg-orange-300 text-white text-base rounded-md cursor-pointer flex items-center justify-center px-2 py-2 mt-2 hover:bg-black hover:text-slate-100 transition duration-1000 ease-in-out"
+              isLoading={isLoading}
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RequestModal;
--- components/Richlux.jsx
+++ components/Richlux.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import VaraComponent from "./Vara";
 import { BiHomeHeart, BiSupport } from "react-icons/bi";
-import { MdOutlineSell } from "react-icons/md";
+import { MdOutlineSell, MdClose } from "react-icons/md";
+import RequestModal from "@/app/modal/request/page";
 function Richlux() {
+  const [requestModal, setRequestModal] = useState(false);
+  const handleRequest = () => {
+    setRequestModal(!requestModal);
+  };
+  const handleClose =()=>{
+    setRequestModal(!requestModal);
+  }
   return (
     <div className="w-screen h-[90vh] bg-gray-900 flex items-center justify-center relative">
       <div className="items-center justify-center flex flex-col md:w-4/5 w-full ">
         <VaraComponent text="Richlux Properties" />
-        <div className="mt-5 flex items-center justify-between md:w-3/5 w-full space-x-3 md:px-0 px-4">
-          <div className="flex flex-col items-center text-white p-3 rounded-lg richshadow richtrans cursor-pointer">
-            <div className="flex items-center justify-center space-x-2">
+        <div className="mt-5 flex items-center justify-between md:w-[50%] w-full space-x-3 md:px-0 px-4">
+          <div
+            className="flex w-full items-center justify-center  text-white p-3 rounded-full richshadow richtrans cursor-pointer"
+            onClick={handleRequest}
+          >
+            <div className="flex flex-col px-4">
+              <span className="text-orange-300 text-xl md:text-3xl font-bold ">
+                Make Your Request
+              </span>
+              <span className="md:text-sm text-xs text-center">
+                What kind of house are you looking for?
+              </span>
+            </div>
+            <BiHomeHeart size={40} className="text-red-400" />
+            {/* <div className="flex items-center justify-center space-x-2">
               <BiHomeHeart size={30} className="text-orange-300" />
               <h2>Buy</h2>
             </div>
             <span className="text-sm text-gray-400 hidden md:flex">
               We sell comfort
-            </span>
+            </span> */}
           </div>
-          <div className="flex flex-col items-center text-white p-3 rounded-lg richshadow richtrans cursor-pointer ">
+          {/* <div className="flex flex-col items-center text-white p-3 rounded-lg richshadow richtrans cursor-pointer ">
             <div className="flex items-center justify-center space-x-2">
               <MdOutlineSell size={30} className="text-orange-300" />
               <h2>Rent</h2>
@@ -34,9 +55,10 @@ function Richlux() {
             <span className="text-sm text-gray-400 hidden md:flex">
               For all enquiries
             </span>
-          </div>
+          </div> */}
         </div>
       </div>
+      <RequestModal visible={requestModal} handleClose={handleClose} CloseIcon={MdClose} />
     </div>
   );
 }
