116fe6098eb24e60cf266584a23e0b757a53a0dd
ajeigbepaul <ajeigbepaul@yahoo.com>
2023-08-27 01:20:42 +0100

    fixing-updated the nextconfig

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
+    reactServerComponents: false,
   },
   images: {
     domains: ["lh3.googleusercontent.com"],
