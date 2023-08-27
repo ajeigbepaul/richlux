ae8bc9560525a7d01357af0d442942ee95b2860e
ajeigbepaul <ajeigbepaul@yahoo.com>
2023-08-27 13:32:38 +0100

    fixing-other commit2

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import Header from "@/components/Header";
+// import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 // import Richlux from "@/components/Richlux";
 export default function Home() {
   return (
     <main className="flex relative flex-col items-center justify-between overflow-x-hidden">
       <div className="w-full bg-gray-900 fixed z-30">
-        <Header />
+        {/* <Header /> */}
       </div>
 
       <div className="w-screen bg-gray-900">
